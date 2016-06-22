510f2f0f3c51b4a15383713fec63a2170f636774
Laser Nite <lasernite@gmail.com>
2016-06-22 16:21:12 -0700

    dropdown as large button to websee

--- visit.js
+++ visit.js
@@ -1,8 +1,7 @@
-
-
-window.onload = function() {
+// On page load send the information that a visit has occured, with just the page URL and Title
+function sendData() {
 	
-	var url = window.location.hostname + window.location.pathname;
+	var url = window.location.href;
 	var title = document.getElementsByTagName("title")[0].innerHTML;
 
 	var data = JSON.stringify({
@@ -13,13 +12,14 @@ window.onload = function() {
 	var xhr = new XMLHttpRequest();
 	xhr.withCredentials = true;
 
-	xhr.addEventListener("readystatechange", function () {
-	  if (this.readyState === 4) {
-	    console.log(this.responseText);
-	  }
-	});
+	// xhr.addEventListener("readystatechange", function () {
+	//   if (this.readyState === 4) {
+	//     console.log(this.responseText);
+	//   }
+	// });
 
-	xhr.open("POST", "https://websee.herokuapp.com/visits/create.json");
+	xhr.open("POST", "http://localhost:3000/visits/create.json");
+	// xhr.open("POST", "https://websee.herokuapp.com/visits/create.json");
 	xhr.setRequestHeader("content-type", "application/json");
 	xhr.setRequestHeader("charset", "utf-8");
 	xhr.setRequestHeader("cache-control", "no-cache");
@@ -28,3 +28,11 @@ window.onload = function() {
 	xhr.send(data);
 
 };
+
+if (window.attachEvent) {
+	window.attachEvent('onload', sendData);
+} else if (window.addEventListener) { 
+	window.addEventListener('load', sendData, false);
+} else {
+	document.addEventListener('load', sendData, false);
+}
