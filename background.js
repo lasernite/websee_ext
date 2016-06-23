527dc3e9b05ce2da88d8baa3e2e2397c3090887e
Laser Nite <lasernite@Lasers-MacBook.local>
2016-06-23 13:19:17 -0700

    URL and Title send fully switched over to background.js, and works on SPAs

--- background.js
+++ background.js
@@ -2,12 +2,66 @@
 chrome.webNavigation.onCompleted.addListener(function(details) {
 	// Get the tab id from which the navigation was made
 	var tabId = details.tabId
-	// Get the tab URL and Title, if it has changed since the last call
+	// Get the tab
 	chrome.tabs.get(tabId, function(tab) {
-		if (window.oldURL != tab.url) {
-			console.log(tab.url);
-			console.log(tab.title);
+		// Send URL and Title, if it has changed since the last call
+		if (window.oldUrl != tab.url) {
+			var url = tab.url;
+			var title = tab.title;
+
+			var data = JSON.stringify({
+			  "url": url,
+			  "title": title
+			});
+
+			var xhr = new XMLHttpRequest();
+			xhr.withCredentials = true;
+
+			// xhr.open("POST", "http://localhost:3000/visits/create.json");
+			xhr.open("POST", "https://websee.herokuapp.com/visits/create.json");
+			xhr.setRequestHeader("content-type", "application/json");
+			xhr.setRequestHeader("charset", "utf-8");
+			xhr.setRequestHeader("cache-control", "no-cache");
+			xhr.setRequestHeader("postman-token", "dce8c16c-477b-198f-d1d2-8b5356ce7ffb");
+
+			xhr.send(data);
 		}
-		window.oldURL = tab.url
+		// Update oldUrl with current url, for next call
+		window.oldUrl = tab.url
 	});
 })
+
+// On page change send new URL and Title
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+	// Get the tab id from which the navigation was made
+	var tabId = details.tabId
+	// Get the tab
+	chrome.tabs.get(tabId, function(tab) {
+		// Send URL and Title, if it has changed since the last call
+		if (window.oldUrl != tab.url) {
+			var url = tab.url;
+			var title = tab.title;
+
+			var data = JSON.stringify({
+			  "url": url,
+			  "title": title
+			});
+
+			var xhr = new XMLHttpRequest();
+			xhr.withCredentials = true;
+
+			// xhr.open("POST", "http://localhost:3000/visits/create.json");
+			xhr.open("POST", "https://websee.herokuapp.com/visits/create.json");
+			xhr.setRequestHeader("content-type", "application/json");
+			xhr.setRequestHeader("charset", "utf-8");
+			xhr.setRequestHeader("cache-control", "no-cache");
+			xhr.setRequestHeader("postman-token", "dce8c16c-477b-198f-d1d2-8b5356ce7ffb");
+
+			xhr.send(data);
+		}
+		// Update oldUrl with current url, for next call
+		window.oldUrl = tab.url
+	});
+})
+
+
