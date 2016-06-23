109b6d6f9fd5d89a4e6b9c5734736517c5c770ef
Laser Nite <lasernite@Lasers-MacBook.local>
2016-06-23 12:47:28 -0700

    extension rebuild with background process and less permissions in progress

--- background.js
+++ background.js
@@ -1,1 +1,13 @@
-// nothing yet
+// On page change send new URL and Title
+chrome.webNavigation.onCompleted.addListener(function(details) {
+	// Get the tab id from which the navigation was made
+	var tabId = details.tabId
+	// Get the tab URL and Title, if it has changed since the last call
+	chrome.tabs.get(tabId, function(tab) {
+		if (window.oldURL != tab.url) {
+			console.log(tab.url);
+			console.log(tab.title);
+		}
+		window.oldURL = tab.url
+	});
+})
