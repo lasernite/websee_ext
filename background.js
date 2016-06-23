bb64ef801993b2354b5ee8338bbd3c853db4b793
Laser Nite <lasernite@Lasers-MacBook.local>
2016-06-23 13:28:58 -0700

    refactored background.js code

--- background.js
+++ background.js
@@ -1,38 +1,14 @@
 // On page change send new URL and Title
 chrome.webNavigation.onCompleted.addListener(function(details) {
-	// Get the tab id from which the navigation was made
-	var tabId = details.tabId
-	// Get the tab
-	chrome.tabs.get(tabId, function(tab) {
-		// Send URL and Title, if it has changed since the last call
-		if (window.oldUrl != tab.url) {
-			var url = tab.url;
-			var title = tab.title;
-
-			var data = JSON.stringify({
-			  "url": url,
-			  "title": title
-			});
-
-			var xhr = new XMLHttpRequest();
-			xhr.withCredentials = true;
-
-			// xhr.open("POST", "http://localhost:3000/visits/create.json");
-			xhr.open("POST", "https://websee.herokuapp.com/visits/create.json");
-			xhr.setRequestHeader("content-type", "application/json");
-			xhr.setRequestHeader("charset", "utf-8");
-			xhr.setRequestHeader("cache-control", "no-cache");
-			xhr.setRequestHeader("postman-token", "dce8c16c-477b-198f-d1d2-8b5356ce7ffb");
-
-			xhr.send(data);
-		}
-		// Update oldUrl with current url, for next call
-		window.oldUrl = tab.url
-	});
+	sendDataIfUrlChange(details)
 })
 
 // On page change send new URL and Title
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+	sendDataIfUrlChange(details);
+})
+
+function sendDataIfUrlChange(details) {
 	// Get the tab id from which the navigation was made
 	var tabId = details.tabId
 	// Get the tab
@@ -62,6 +38,6 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
 		// Update oldUrl with current url, for next call
 		window.oldUrl = tab.url
 	});
-})
+}
 
 
